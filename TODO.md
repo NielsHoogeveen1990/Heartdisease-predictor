1fe92d1b9545fb2ce2c10d043a1709eac4e4cf2c
Niels Hoogeveen <nielshoogeveen@Nielss-MacBook-Pro.local>
2020-10-01 22:26:37 +0200

    created upload function for S3 to save ML models

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# Backlog
+
+1. Upload trained ML model to S3 bucket: done
+2. Read trained ML model in API from S3 bucket --> latest model based 
+on create date
+3. API based on AWS Lambda with Serverless framework
+4. Read CSV data from S3 bucket --> Take the latest file 
+5. Create a AWS Lambda function based on S3 event trigger
+6. Write batch prediction results to S3 bucket
+
--- src/heartdisease/cli.py
+++ src/heartdisease/cli.py
@@ -18,3 +18,13 @@ def main():
 def train_model(data_path, model_version):
     models_utils.run(data_path, model_version)
     logger.info('Finished with training the final model.')
+
+
+@main.command()
+@click.option("--data-path", type=click_pathlib.Path(exists=True))
+@click.option("--model-version", type=float)
+@click.option("--access", type=str)
+@click.option("--secret", type=str)
+def write_model_aws(data_path, model_version, access, secret):
+    models_utils.write_to_S3(data_path, model_version, access, secret)
+    logger.info('Finished with training the model and writing to S3.')
